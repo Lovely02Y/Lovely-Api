d7f9439a939ded1e6ac45874eef4281cb40339ca
Loovely-02 <HenTai02Y@gmail.com>
2024-07-27 18:56:19 +0800

    Lovely

--- app.js
+++ app.js
@@ -5,10 +5,15 @@ const path = require('path');
 const app = express();
 const port = 7007; // 要更改为你需要的对外端口号
 
-const imagesFolder = ''; // 要更改为你本地存储图片的文件夹路径
+const imgs = '/opt/1panel/apps/lsky-pro/lsky-pro/data/storage/app/uploads/2'; // 要更改为你本地存储图片的文件夹路径
+const sese = '/opt/1panel/apps/lsky-pro/lsky-pro/data/storage/app/uploads/3'; // 要更改为你本地存储图片的文件夹路径
 
-app.get('', (req, res) => {
-    fs.readdir(imagesFolder, (err, files) => {
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/resources/index.html');
+});
+
+app.get('/imgs', (req, res) => {
+    fs.readdir(imgs, (err, files) => {
         if (err) {
             res.status(500).send('Internal Server Error');
             return;
@@ -16,7 +21,22 @@ app.get('', (req, res) => {
 
         const randomIndex = Math.floor(Math.random() * files.length);
         const randomImage = files[randomIndex];
-        const imagePath = path.join(imagesFolder, randomImage);
+        const imagePath = path.join(imgs, randomImage);
+
+        res.sendFile(imagePath);
+    });
+});
+
+app.get('/sese', (req, res) => {
+    fs.readdir(sese, (err, files) => {
+        if (err) {
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+
+        const randomIndex = Math.floor(Math.random() * files.length);
+        const randomImage = files[randomIndex];
+        const imagePath = path.join(sese, randomImage);
 
         res.sendFile(imagePath);
     });
