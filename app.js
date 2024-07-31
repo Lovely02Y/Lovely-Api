01e151710978878779d0cda602a37cb4efa74a8b
Loovely-02 <HenTai02Y@gmail.com>
2024-07-31 20:21:14 +0800

    Lovely

--- app.js
+++ app.js
@@ -39,10 +39,15 @@ const limiter = rateLimit({
   message: '你小子不准爬了哦！！！ 你爬的太多啦！！！'
 });
 
+// 获取当前时间并格式化
+const now = new Date();
+const dateString = now.toLocaleDateString();
+const timeString = now.toLocaleTimeString();
+
 // 中间件函数：检查是否在白名单或黑名单中
 async function checkLists(req, res, next) {
     const clientIp = req.ip;
-    console.log(`访问IP: ${clientIp}`);
+    console.log(`日期: ${dateString} 时间: ${timeString} 访问IP: ${clientIp}`);
 
     const { whitelist, blacklist } = await loadConfig();
 
